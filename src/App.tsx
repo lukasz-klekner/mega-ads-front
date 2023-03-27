2f2763a39bcfde1b98a61e47d8732254a61f3cd0
Luki Klekner <lukaszklekner@gmail.com>
2023-03-27 08:58:34 +0200

    feat: create basic structure of main view

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import './App.css'
+
 export const App = () => {
-  
+
   return (
-    <div>App...</div>
+    <>
+      <header>
+        <h1>
+          <span>Mega</span> Ogłoszenia
+        </h1>
+        <button>Dodaj ogłoszenie</button>
+        <div className="search">
+          <input type="text" />
+          <button>Szukaj</button>
+        </div>
+      </header>
+
+      <div className="map"></div>
+    </>
   );
 }
