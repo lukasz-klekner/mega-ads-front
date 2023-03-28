d09951b5b0309da3c9668b2333505b29ee3ab51e
Luki Klekner <lukaszklekner@gmail.com>
2023-03-28 12:43:17 +0200

    feat: instal leaflet and cnofigure a map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,7 +1,17 @@
+import { MapContainer, TileLayer } from "react-leaflet"
+
+import "leaflet/dist/leaflet.css"
 import "./Map.css"
 
 export const Map = () => {
     return (
-        <div className="map"></div>
+        <div className="map">
+            <MapContainer center={[50.2, 18.99]} zoom={20}>
+                <TileLayer 
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='https://www.openstreetmap.org/opyright'>OpenStreetMap</a> & contributors"
+                 />
+            </MapContainer>
+        </div>
     )
 }
