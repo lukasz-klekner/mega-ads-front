dc0ee00f6c15b2bf6e40314c054a058dff2abdbf
Luki Klekner <lukaszklekner@gmail.com>
2023-03-28 12:59:46 +0200

    feat: add marker and configure default icon

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,4 +1,6 @@
-import { MapContainer, TileLayer } from "react-leaflet"
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+
+import "../../utils/marker-icon"
 
 import "leaflet/dist/leaflet.css"
 import "./Map.css"
@@ -11,6 +13,11 @@ export const Map = () => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href='https://www.openstreetmap.org/opyright'>OpenStreetMap</a> & contributors"
                  />
+                 <Marker position={[50.2, 18.99]}>
+                    <Popup>
+                        <p>IT focus</p>
+                    </Popup>
+                 </Marker>
             </MapContainer>
         </div>
     )
