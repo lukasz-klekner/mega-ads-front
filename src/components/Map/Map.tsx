3a36f6bac128029fce37dcaf010742e198f8e0c6
Luki Klekner <lukaszklekner@gmail.com>
2023-03-31 08:44:53 +0200

    feat: fetch ads from api and display them on map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,11 +1,26 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet"
+import { SimpleAdItem } from "types"
 
 import "../../utils/marker-icon"
 
 import "leaflet/dist/leaflet.css"
 import "./Map.css"
+import { useEffect, useState } from "react"
+import { useSearchContext } from "../../context/searchContext"
 
 export const Map = () => {
+    const [ads, setAds] = useState<SimpleAdItem[]>([])
+    const searchContext = useSearchContext()
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch('http://localhost:3001/ad/search')
+            const data = await response.json()
+
+            setAds(data)
+        })()
+    }, [searchContext?.search])
+
     return (
         <div className="map">
             <MapContainer center={[50.2, 18.99]} zoom={20}>
@@ -13,11 +28,15 @@ export const Map = () => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href='https://www.openstreetmap.org/opyright'>OpenStreetMap</a> & contributors"
                  />
-                 <Marker position={[50.2, 18.99]}>
-                    <Popup>
-                        <p>IT focus</p>
-                    </Popup>
-                 </Marker>
+                 {
+                    ads.map(({ _id, lat, lng}) => (
+                        <Marker key={_id.toString()} position={[lat, lng]}>
+                            <Popup>
+                                <p>{_id.toString()}</p>
+                            </Popup>
+                        </Marker>
+                    ))
+                 }
             </MapContainer>
         </div>
     )
